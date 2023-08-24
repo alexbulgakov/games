f0d0fc799003e5aa1fba35c42527b4371b53f4e5
alexbulgakov <alexbulgakov@hotmail.com>
2023-08-25 05:08:34 +0700

    chore(project): created project in CRA and added dependencies

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import appStyles from './app.module.css';
+
+function App() {
+    return (
+        <>
+            <div className={appStyles.app}>
+
+            </div>
+        </>
+    )
+}
+
+export default App;
