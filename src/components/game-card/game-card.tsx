972f6dd3f820fecf5b46e020d550d333bf264b36
alexbulgakov <alexbulgakov@hotmail.com>
2023-08-31 23:17:29 +0700

    refactor(gameCard): removed button, added click-to-card transition

--- src/components/game-card/game-card.tsx
+++ src/components/game-card/game-card.tsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, Typography } from 'antd';
 import { formatDate } from '../../utils/utils';
 import cardStyles from './game-card.module.css';
 import { TGame } from '../../utils/types';
-
+const {Text} = Typography;
 interface IGameCardProps {
     game: TGame;
 }
@@ -12,36 +12,38 @@ const GameCard: React.FC<IGameCardProps> = ({ game }) => {
     const { title, release_date, publisher, genre, thumbnail, id } = game;
 
     return (
-        <div className={cardStyles.gameCardContainer}>
-            <div className={cardStyles.gameCardTop}>
-                <img className={cardStyles.gameCardImage} src={thumbnail} alt={title} />
-                <div className={cardStyles.gameCardInfo}>
-                    <table className={cardStyles.gameCardInfoTable}>
-                        <tbody>
-                            <tr>
-                                <td>Release Date</td>
-                                <td>{formatDate(release_date)}</td>
-                            </tr>
-                            <tr>
-                                <td>Publisher</td>
-                                <td>{publisher}</td>
-                            </tr>
-                            <tr>
-                                <td>Genre</td>
-                                <td>{genre}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+        <Link to={`/game/${id}`}>
+            <div className={cardStyles.gameCardContainer}>
+                <div className={cardStyles.gameCardTop}>
+                    <img className={cardStyles.gameCardImage} src={thumbnail} alt={title} />
+                    <div className={cardStyles.gameCardInfo}>
+                        <table className={cardStyles.gameCardInfoTable}>
+                            <tbody>
+                                <tr>
+                                    <td>Release Date</td>
+                                    <td>{formatDate(release_date)}</td>
+                                </tr>
+                                <tr>
+                                    <td>Publisher</td>
+                                    <td>{publisher}</td>
+                                </tr>
+                                <tr>
+                                    <td>Genre</td>
+                                    <td>{genre}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
-            <div className={cardStyles.gameCardBottom}>
-                <div className={cardStyles.gameCardTitle}>{title}</div>
-                <Link to={`/game/${id}`}>
-                    <Button type="primary" className={cardStyles.gameCardButton} >Learn More</Button>
-                </Link>
-            </div>
+                <div className={cardStyles.gameCardBottom}>
+                    <div className={cardStyles.gameCardTitle}>{title}</div>
 
-        </div>
+                    <Text  className={cardStyles.gameCardClick} >click to learn more</Text>
+
+                </div>
+
+            </div>
+        </Link>
     );
 }
 
