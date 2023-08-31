25763d730b60714fafefab61b429cef3087d8ecd
alexbulgakov <alexbulgakov@hotmail.com>
2023-08-31 13:38:57 +0700

    fix(backend): updated port

--- backend/backend.js
+++ backend/backend.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 const baseUrl = 'https://free-to-play-games-database.p.rapidapi.com/api';
 
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,12 +1,4 @@
-export const baseUrl: string = 'http://localhost:3002/api';
-
-// export const options: RequestInit = {
-//     method: 'GET',
-//     headers: {
-//         'X-RapidAPI-Key': '88df9d9bbdmsh86242f238a476adp197abfjsn410848bc2344',
-//         'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-//     }
-// };
+export const baseUrl = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
 
 export let retries: number = 3;
 
